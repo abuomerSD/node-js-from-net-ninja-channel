67655b48ea874a37133e65cb06b3208726ae118b
Eltayeb <tayeb1293@yahoo.com>
2022-08-15 06:55:06 +0200

    basic routing

--- lesson-4-server/server.js
+++ lesson-4-server/server.js
@@ -1,7 +1,33 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((request, response) => {
-  console.log("request made");
+  console.log(request.url, request.method);
+
+  response.setHeader("Content-Type", "text/html");
+
+  let path = "./views/";
+  switch (request.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      response.end();
+    } else {
+      response.write(data);
+      response.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
