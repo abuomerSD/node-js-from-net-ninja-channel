4fcee0da99f3b9f581f59fd27d5d9536fd7b11a3
Eltayeb <tayeb1293@yahoo.com>
2022-08-19 22:08:34 +0200

    middleware

--- lesson-8-view-engine/app.js
+++ lesson-8-view-engine/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.listen(3000);
@@ -7,12 +8,24 @@ app.listen(3000);
 
 app.set("view engine", "ejs");
 
+//middleware and static files
+
+app.use(express.static("public"));
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  console.log("request made");
+  next();
+});
+
 app.get("/", (req, res) => {
+  let name = " eltayeb ibrahim";
   const blogs = [
     { title: "Learn Html", snippet: "Hyber text markup language" },
     { title: "Learn CSS", snippet: "Cascadin Style Sheet " },
   ];
-  res.render("index", { title: "eltayeb", blogs: blogs });
+  res.render("index", { blogs: blogs, title: name });
 });
 
 app.get("/about", (req, res) => {
